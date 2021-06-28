fe33ded42c415a769afbdbf044f34f61635b2288
Enrico <bravienrico@yahoo.it>
2021-06-28 11:13:04 +0200

    2 exercises

---
+++ exercises/es_cbc_vs_ecb_v3/mysecrets.py
@@ -0,0 +1,4 @@
+ecb_oracle_key = b'\x1e\x86\x114\x0b\x8d6k`\xb1\xdc\xb5\xa9\xc7,\xe8A\xe2\x1c\x0bk\x93Lc\xc0\xa9\xce\xae\xcc.z\xd2'
+
+HOST = 'localhost'   # Symbolic name, meaning all available interfaces
+PORT = 12342
